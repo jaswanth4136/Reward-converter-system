15fdf65671e1a25338c4b61e93e1364044b501e3
Challa Naga Jaswanth Kumar <ch.jaswanth4136@gmail.com>
2024-07-30 22:11:55 +0530

    Created RewardValue with required methods

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,21 @@
+public class RewardValue {
+    private double cashValue;
+    private static final double TO_MILES = 0.01;
+    private static final double TO_CASH = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+    public RewardValue(double miles, boolean isMiles) {
+        if (isMiles) {
+            this.cashValue = miles * TO_CASH;
+        }
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+    public double getMilesValue() {
+        return cashValue / TO_MILES;
+    }
+}
